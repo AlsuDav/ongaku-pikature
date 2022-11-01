a1531cbb89c4de47809b6403d899227934602998
DavletgareevaAR <adavletgareeva@js.team>
2022-11-01 14:31:03 +0300

    init project

---
+++ ongaku-pikature/src/main/java/ru/itis/ongakupikature/OngakuPikatureApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.ongakupikature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngakuPikatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngakuPikatureApplication.class, args);
+	}
+
+}
---
+++ ongaku-pikature/src/test/java/ru/itis/ongakupikature/OngakuPikatureApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.ongakupikature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngakuPikatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
