6dc966b3ea78eaf3994eb9c0f918b6f144f40351
Gulshat Galimova <ggalimova@jc.team>
2022-11-24 21:39:03 +0300

    OP-3: delete unused method

--- ongaku-pikature/src/main/java/ru/itis/ongakupikature/filestorage/dto/LoadResult.java
+++ ongaku-pikature/src/main/java/ru/itis/ongakupikature/filestorage/dto/LoadResult.java
@@ -9,16 +9,7 @@ public sealed interface LoadResult {
 
     sealed interface Failed extends LoadResult {
 
-        String message();
-
         record FileNotLoaded(String message) implements Failed {
-            @Override
-            public FileUuid fileUuid() {
-                return null;
-            }
-        }
-
-        record CannotReadBytes(String message) implements Failed {
             @Override
             public FileUuid fileUuid() {
                 return null;
