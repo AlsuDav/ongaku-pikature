c4d5fce1d022e9014e8ad1b5d0d07e18671a6413
Gulshat Galimova <ggalimova@jc.team>
2022-12-02 23:23:29 +0300

    OP-3: delete UserNotFoundException

--- ongaku-pikature/src/main/java/ru/itis/ongakupikature/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.itis.ongakupikature.exception;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
